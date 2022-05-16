5bbc08df20dae767a5e02df1e1b94375ff528313
vivek-mahajan-xero <vivek.mahajan@xero.com>
2022-05-16 11:21:00 +1000

    Add spread example

--- spread/index.js
+++ spread/index.js
@@ -1,66 +1,26 @@
-/**
- * Unpack an array into all its elements.
- * It doesn't create new variables.
- * As a result we can only use it in places.
- * We would otherwise write values separated by a comma.
- *
- * Works on all iterables plus objects.
- * Used only in building arrays or passing values into a function.
- */
+// Inside of function call
+// Spread out the items in this array as individual arguments
+const temperatures = [45, 23, 1, 2, -4];
+console.log(Math.min(...temperatures));
+console.log(...temperatures);
 
-const arr = [7, 8, 9];
-const newArr = [1, 2, ...arr];
-console.log(...newArr);
+// Spread works on all iterables
+console.log(...'HELLO!');
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  startMenu: ['Focacia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// Create a new array
+const parents = ['Swaraj', 'Usha'];
+const kids = ['Rajiv', 'Shaveta', 'Vivek'];
+const fullFamily = [...parents, ...kids];
+console.log(fullFamily);
 
-  order: function (starterIndex, mainIndex) {
-    return [this.startMenu[starterIndex], this.mainMenu[mainIndex]]
-  },
+// Create a shallow copy of an array
+const copies = [...fullFamily];
+copies.push('Purnima');
+console.log(...fullFamily);
+console.log(...copies);
 
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(ing1, ing2, ing3);
-  }
-};
-
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(...newMenu);
-
-// copy array
-const mainMenuCopy = [...restaurant.mainMenu];
-
-// join 2 array
-const menu = [...restaurant.startMenu, ...restaurant.mainMenu];
-
-const str = 'Jonas';
-const letters = [...str, ' ', 'S.'];
-console.log(letters);
-
-const ingredients = ['mushrooms', 'cheese', 'asparagus'];
-restaurant.orderPasta(...ingredients);
-
-// Objects
-const newRestaurant = {
-  ...restaurant,
-  founder: 'Guiseppe',
-  name: 'New Classico Italiano'
-};
-
-console.log(newRestaurant);
-
-// shallow copy of an object
-const restaurantCopy = {...restaurant};
-restaurantCopy.name = 'Ristorante Roma';
-console.log(restaurantCopy.name);
-console.log(restaurant.name);
-
-
-
-
-
-
+// Create a new object
+const lion = {hasTail: true, legs: 4};
+const eagle = {canFly: true};
+const hybrid = {name: 'Gryphon', ...lion, ...eagle};
+console.log(hybrid);
